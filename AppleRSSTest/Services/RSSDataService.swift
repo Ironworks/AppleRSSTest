795f5000c222b6a2bd40ae6edf5a94f24cd34382
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2018-09-23 22:03:05 +0100

    Initial DataServices Setup

---
+++ AppleRSSTest/Services/RSSDataService.swift
@@ -0,0 +1,9 @@
+//
+//  RSSDataService.swift
+//  AppleRSSTest
+//
+//  Created by Trevor Doodes on 23/09/2018.
+//  Copyright © 2018 Ironworks Media Limited. All rights reserved.
+//
+
+import Foundation
---
+++ AppleRSSTestTests/RSSDAtaServiceTests.swift
@@ -0,0 +1,21 @@
+//
+//  RSSDAtaServiceTests.swift
+//  AppleRSSTestTests
+//
+//  Created by Trevor Doodes on 23/09/2018.
+//  Copyright © 2018 Ironworks Media Limited. All rights reserved.
+//
+
+import XCTest
+
+class RSSDAtaServiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
