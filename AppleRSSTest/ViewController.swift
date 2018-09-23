46578886d3d0298f3fec0f12fb4dbd0e51cea270
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2018-09-23 21:55:38 +0100

    Initial Commit

---
+++ AppleRSSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleRSSTest
+//
+//  Created by Trevor Doodes on 23/09/2018.
+//  Copyright © 2018 Ironworks Media Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
