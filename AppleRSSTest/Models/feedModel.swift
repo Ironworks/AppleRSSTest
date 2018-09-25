b433dfe5573e2f303eaa237ae03a441d21fabf1b
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2018-09-25 09:16:15 +0100

    Adds initial implementation of feed model to project.

---
+++ AppleRSSTest/Models/feedModel.swift
@@ -0,0 +1,51 @@
+//
+//  feedModel.swift
+//  AppleRSSTest
+//
+//  Created by Trevor Doodes on 24/09/2018.
+//  Copyright © 2018 Ironworks Media Limited. All rights reserved.
+//
+
+import Foundation
+
+struct feed: Codable {
+    let title: String
+    let id: String
+    let author: Author
+    let links: [Link]
+    let copyright: String
+    let country: String
+    let icon: String
+    let updated: String
+    let results: [Result]
+}
+
+struct Author: Codable {
+    let name: String
+    let uri: String
+}
+
+struct Link: Codable {
+    let primary: String
+    let alternate: String
+}
+
+struct Result: Codable {
+    let artistName: String
+    let id: String
+    let releaseDate: String
+    let name: String
+    let kind: String
+    let copyright: String
+    let artworkUrl100: String
+    let genres: [Genre]
+    let url: String
+}
+
+struct Genre: Codable {
+    let id: String
+    let name: String
+    let url: String
+}
+
+
